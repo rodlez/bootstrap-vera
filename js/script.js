5ef9e2b1eb75bc390213ef90b3909a3d9dff80ac
rodlez <xavier.rodlez@gmail.com>
2024-07-30 19:54:05 +0200

    Finish Video Modal using JS script

--- js/script.js
+++ js/script.js
@@ -35,3 +35,28 @@ function userScroll(startPixel, colorChange) {
 
 // Once the DOM id loaded the functions will be executed
 document.addEventListener('DOMContentLoaded', userScroll(50, 'bg-dark'));
+
+// Video Modal
+const videoBtn = document.querySelector('.video-btn');
+const videoModal = document.querySelector('#videoModal');
+const video = document.querySelector('#video');
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener('click', () => {
+    videoSrc = videoBtn.getAttribute('data-bs-src');
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener('shown.bs.modal', () => {
+    video.setAttribute(
+      'src',
+      videoSrc + '?autoplay=1;modestbranding=1;showInfo=0'
+    );
+  });
+
+  videoModal.addEventListener('hide.bs.modal', () => {
+    video.setAttribute('src', videoSrc);
+  });
+}
