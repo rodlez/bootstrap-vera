861766a130d4efc75c4b9fa6eca1b84be9030c28
rodlez <xavier.rodlez@gmail.com>
2024-07-30 18:06:07 +0200

    Fix Navigation scroll
    JS navigation scroll, and show social in small screens

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ if (checkReplace !== null) {
     autoRun: true,
   });
 }
+
+/**  Make the navigation bar change color when we scroll more than X pixels vertically
+ * @param {int} startPixel Number of pixels when the navbar will change color
+ * @param {string} colorChange Color of navbar background
+ * */
+function userScroll(startPixel, colorChange) {
+    const navbar = document.querySelector('.navbar');
+    // To Show or Hide the top button when we scroll
+    const toTopBtn = document.querySelector('#to-top');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > startPixel) {
+            navbar.classList.add(colorChange);           
+            navbar.classList.add('navbar-sticky');
+            toTopBtn.classList.add('show');
+        } else {
+            navbar.classList.remove(colorChange);           
+            navbar.classList.remove('navbar-sticky');
+            toTopBtn.classList.remove('show');
+        }
+    });
+}
+
+// Once the DOM id loaded the functions will be executed
+document.addEventListener('DOMContentLoaded', userScroll(50, 'bg-dark'));
